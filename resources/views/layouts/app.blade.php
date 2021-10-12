00b6cd7f721649ae1753582a6131f8e474a84f4f
thihaaung3 <thihaaung@htut.com>
2021-10-12 09:36:22 +0630

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'VVIP NINE') }}</title>
+    <title>VVIP NINE</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
