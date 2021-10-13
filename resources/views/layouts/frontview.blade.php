2d822e03e436a0898fb4bbb440add7d0216f2c2c
thihaaung3 <thihaaung@htut.com>
2021-10-13 21:32:51 +0630

    fix

--- resources/views/layouts/frontview.blade.php
+++ resources/views/layouts/frontview.blade.php
@@ -709,28 +709,34 @@
         </div>
         <div id="loading" style="display:block;">
             <div class="container justify-content-center d-flex">
-            <img src="../images/logo.jpeg" alt="" width="350" height="350" class="main_logo mt-4">
-            <svg class="load" version="1.1" id="L4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-            viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
-                <animate
-                attributeName="opacity"
-                dur="1s"
-                values="0;1;0"
-                repeatCount="indefinite"
-                begin="0.1"/>   fas fa-check fill="rgb(217,181,81)" stroke="none" cx="26" cy="50" r="6">
-                <animate
-                attributeName="opacity"
-                dur="1s"
-                values="0;1;0"
-                repeatCount="indefinite" 
-                begin="0.2"/>      fas fa-check fill="rgb(217,181,81)" stroke="none" cx="46" cy="50" r="6">
-                <animate
-                attributeName="opacity"
-                dur="1s"
-                values="0;1;0"
-                repeatCount="indefinite" 
-                begin="0.3"/>    fas fa-check>
-            </svg>
+                <img src="../images/logo.jpeg" alt="" width="350" height="350" class="main_logo mt-4">
+                <svg class="load" version="1.1" id="L4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
+                <circle fill="rgb(217,181,81)" stroke="none" cx="6" cy="50" r="6">
+                    <animate
+                    attributeName="opacity"
+                    dur="1s"
+                    values="0;1;0"
+                    repeatCount="indefinite"
+                    begin="0.1"/>    
+                </circle>
+                <circle fill="rgb(217,181,81)" stroke="none" cx="26" cy="50" r="6">
+                    <animate
+                    attributeName="opacity"
+                    dur="1s"
+                    values="0;1;0"
+                    repeatCount="indefinite" 
+                    begin="0.2"/>       
+                </circle>
+                <circle fill="rgb(217,181,81)" stroke="none" cx="46" cy="50" r="6">
+                    <animate
+                    attributeName="opacity"
+                    dur="1s"
+                    values="0;1;0"
+                    repeatCount="indefinite" 
+                    begin="0.3"/>     
+                </circle>
+                </svg>
             </div>
         </div>
         <!-- body ends -->
