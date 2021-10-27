b12b711443c51ad9a823027a0a1404d37fd22c0a
thihaaung3 <thihaaung@htut.com>
2021-10-27 08:52:27 +0630

    fix

--- resources/views/vvip_customers/select_view.blade.php
+++ resources/views/vvip_customers/select_view.blade.php
@@ -4,7 +4,7 @@
 
 <input type="text" id="request" value="{{ $data_module->request_name }}" hidden>
 <input type="text" id="user_id" value="{{ $data_module->user_id }}" hidden>
-<h5 style="text-align:center; color: #fff; font-size: 50px; top: 50%; position:absolute; left: 23%;"><a href="" id="view_value"></a></h5>
+<a id="view_value" href="" style="text-align:center; color: #fff; font-size: 50px; top: 50%; position:absolute; left: 23%;"></h5>
 
 @section('script')
 <script>
