8c9ccc58a50b40f06d19c547c812b79bac262ef3
thihaaung3 <thihaaung@htut.com>
2021-10-29 13:25:22 +0630

    fix

--- resources/views/vvip_customers/select_view.blade.php
+++ resources/views/vvip_customers/select_view.blade.php
@@ -90,8 +90,8 @@
                     if(value['active'] == 1){
                         var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
                         if (isMobile) {
-                            var app_url = "firefox://open-url?url=domain";
-                            window.open(app_url,null);
+                            const app_url = "intent://facebook.com/#Intent;scheme=https;package=com.facebook.android;end";
+                            window.location.replace(app_url);
                         }
                     }
                 });
