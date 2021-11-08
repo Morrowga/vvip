ec724ad8dc71fb037c0e3d23db6dbf5e30574e82
thihaaung3 <thihaaung@htut.com>
2021-11-08 09:53:32 +0630

    fix

--- resources/views/vvip_customers/select_view.blade.php
+++ resources/views/vvip_customers/select_view.blade.php
@@ -222,7 +222,7 @@
                             const app_url = "intent://"+ value['url'] +"#Intent;scheme=https;package="+ package +";end";
                             window.location.replace(app_url);
                         } else if(isIOS){
-                            window.location.replace("facebook://nhyoe.thwayyaung/"); 
+                            window.location.replace("instagram://"); 
                             // setTimeout(function () {
                             //     window.location.replace("https://itunes.apple.com/app/facebook/id12345678"); 
                             // }, 2000);
