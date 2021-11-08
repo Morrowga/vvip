57e00453a5640731829baaf161b09ba4a92e8fd1
thihaaung3 <thihaaung@htut.com>
2021-11-08 14:41:10 +0630

    fix

--- resources/views/vvip_customers/select_view.blade.php
+++ resources/views/vvip_customers/select_view.blade.php
@@ -229,7 +229,7 @@
                                 var url = "vnd.youtube://channel/UCVSNWq6MTXBZuIJ6iZ1z6Ng";
                                 window.location.replace(url); 
                             } else if(value['name'] == "Tiktok"){
-                                var url = "vt.tiktok://" + host;
+                                var url = "www.tiktok.com/" + host;
                                 window.location.replace(url); 
                             }
                         }
