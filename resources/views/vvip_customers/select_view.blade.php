8d06583071db1afb46f54b33f6538a35ff3c9059
thihaaung3 <thihaaung@htut.com>
2021-11-08 11:08:25 +0630

    fix

--- resources/views/vvip_customers/select_view.blade.php
+++ resources/views/vvip_customers/select_view.blade.php
@@ -223,7 +223,7 @@
                             window.location.replace(app_url);
                         } else if(isIOS){
                             // var path = "nhyoe.thwayyaung";
-                            window.location.replace("com.LinkedIn.LinkedIniOSApp://in/thiha-aung-00852b194"); 
+                            window.location.replace("linkedin://in/thiha-aung-00852b194"); 
                             // setTimeout(function () {
                             //     window.location.replace("https://itunes.apple.com/app/facebook/id12345678"); 
                             // }, 2000);
