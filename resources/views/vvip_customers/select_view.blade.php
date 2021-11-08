75331a812fe35770e40e15723fe5ad1c31e34cb3
thihaaung3 <thihaaung@htut.com>
2021-11-08 10:04:33 +0630

    fix

--- resources/views/vvip_customers/select_view.blade.php
+++ resources/views/vvip_customers/select_view.blade.php
@@ -222,7 +222,7 @@
                             const app_url = "intent://"+ value['url'] +"#Intent;scheme=https;package="+ package +";end";
                             window.location.replace(app_url);
                         } else if(isIOS){
-                            window.location.replace("instagram://aung_pyae12"); 
+                            window.location.replace("fb://"); 
                             // setTimeout(function () {
                             //     window.location.replace("https://itunes.apple.com/app/facebook/id12345678"); 
                             // }, 2000);
