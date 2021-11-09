481f07b31813a29946acd6b01b017d5ed6486ffa
thihaaung3 <thihaaung@htut.com>
2021-11-09 10:51:05 +0630

    fix

--- resources/views/vvip_customers/select_view.blade.php
+++ resources/views/vvip_customers/select_view.blade.php
@@ -225,8 +225,9 @@
                                     var url = "fb://profile/?id=" + host;
                                     window.location.replace(url); 
                                     setTimeout(function () {
-                                        if (new Date().valueOf() - now > 100) return;
-                                        window.location.replace("https://itunes.apple.com/app/facebook/id284882215");
+                                        if (new Date().valueOf() - now > 100) {
+                                            window.location.replace("https://itunes.apple.com/app/facebook/id284882215");
+                                        }
                                     }, 25);
                                 } else {
                                     var url = "https://www.facebook.com/" + host;
