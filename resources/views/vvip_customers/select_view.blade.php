fdfd1ae8cf4a3e96d2f07c2b7a3465dfd20e647f
thihaaung3 <thihaaung@htut.com>
2021-11-10 16:38:44 +0630

    fix

--- resources/views/vvip_customers/select_view.blade.php
+++ resources/views/vvip_customers/select_view.blade.php
@@ -235,7 +235,7 @@
                                         }
                                     }, 300);
                                 } else if(isAndroid){
-                                    var url = "intent://www.facebook.com/profile/"+ host +"#Intent;package=com.facebook.katana;scheme=fb;end";
+                                    var url = "intent://profile/"+ host +"#Intent;package=com.facebook.katana;scheme=fb;end";
                                     window.location = url;
                                 } else {
                                     var url = "https://www.facebook.com/" + host;
