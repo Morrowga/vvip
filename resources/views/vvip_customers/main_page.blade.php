5bdc1dbdbc07f8eb9080bb855676d8f0ff6f5488
thihaaung3 <thihaaung@htut.com>
2021-12-03 11:24:48 +0630

    fix

--- resources/views/vvip_customers/main_page.blade.php
+++ resources/views/vvip_customers/main_page.blade.php
@@ -22,6 +22,7 @@
                                 <h1 class="logo-text">VVIP NINE</h1>
                                 <video height="250" preload class="text-center d-flex justify-content-center" id="video_one" loop autoplay muted>
                                     <source src="../images/VVIP 9 Still 4 sec.mp4" type="video/mp4" id="video_one">
+                                    <source src="../images/VVIP 9 Still 4 sec.mp4" type="video/ogg" id="video_one">
                                 </video>
                                 <p>{{__('website.Everything')}}</p>
                                 <div class="extra-space-l"></div>
@@ -211,7 +212,16 @@
         <!-- End contact section -->
 @section('script')
 <script type="text/javascript">
-    
+    vid = document.getElementById("video_one");
+    //listen for CANPLAY event
+      vid.addEventListener("canplay", function() {
+    //    console.log("oncanplay");
+       setTimeout(function() {
+    //     console.log("play");
+    //Hit PLAY when video fully loaded
+        vid.play();
+       }, 500);
+    });
 
     $('#contact_btn').on('click', function(e){ 
         e.preventDefault();
