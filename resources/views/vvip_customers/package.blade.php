b9172eb3d4f5ec34d43d1ec9bb6f9b1b54af3a85
thihaaung3 <thihaaung@htut.com>
2021-12-20 09:52:27 +0630

    fix

--- resources/views/vvip_customers/package.blade.php
+++ resources/views/vvip_customers/package.blade.php
@@ -403,7 +403,7 @@
     
 
     <div class="modal fade bd-example-modal-lg" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg modal-vertical-centered">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-body">
                     <div class="row" style="margin-top: 60px !important;">
@@ -425,7 +425,7 @@
 
 
     <div class="modal fade bd-example-modal-lg" id="logoModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg modal-vertical-centered">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content" style="background-color: #fff !important;">
                 <div class="modal-body" style="background-color: #fff !important;">
                     <div class="col-md-12" style="margin-top: 60px !important;">
@@ -445,7 +445,7 @@
     </div>
 
     <div class="modal fade bd-example-modal-lg" id="nameTextModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg modal-vertical-centered">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content" style="background-color: #fff !important;">
                 <div class="modal-body" style="background-color: #fff !important;">
                     <div class="col-md-12" style="margin-top: 60px !important;">
@@ -462,7 +462,7 @@
     </div>
 
     <div class="modal fade bd-example-modal-lg" id="descriptionTextModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg modal-vertical-centered">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content" style="background-color: #fff !important;">
                 <div class="modal-body" style="background-color: #fff !important;">
                     <div class="col-md-12" style="margin-top: 60px !important;">
@@ -480,7 +480,7 @@
 
 
     <div class="modal fade bd-example-modal-lg" id="confirm_modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content" id="modal_pdf">
                 <div class="modal-body  text-center">
                     <div class="col-md-12 mt-2">
